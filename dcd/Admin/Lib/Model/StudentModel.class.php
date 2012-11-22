5d79c82d32927b5721c7cf2e723c0cab0c152f45
sxc <sheng_xc@126.com>
2012-11-22 22:10:49 +0800

    student就差js了

--- dcd/Admin/Lib/Model/StudentModel.class.php
+++ dcd/Admin/Lib/Model/StudentModel.class.php
@@ -3,6 +3,8 @@ class StudentModel extends Model{
 	protected $_validate=array(
 		array('name','require','姓名不能为空！',0,'',3),
 		array('id','require','学号不能为空',1,'',3),
+		array('gender','require','性别不能为空',2,'',3),
+		array('teacher','require','导师工号不能为空',3,'',3),
 	);
 }
 ?>
---
+++ dcd/Public/Js/student.js
@@ -0,0 +1,7 @@
+function review()
+{
+	var ids = document.getElementsByName("id")
+	var id = ids[0]
+	alert(id.value)
+	return false
+}
