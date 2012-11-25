bca86e47d6bd62a1e7c3d3c8c3ea333c7aafa6f5
sxc <sheng_xc@126.com>
2012-11-25 23:15:15 +0800

    complete前台人员管理

---
+++ dcd/DCDApp/Lib/Action/PeopledetailAction.class.php
@@ -0,0 +1,20 @@
+﻿<?php
+class PeopledetailAction extends BaseAction{
+	public function index()
+	{
+		$cate = $_GET["category"];
+		$conf["id"] = $_GET["id"];
+		$p = M($cate)->where($conf)->select();
+		$this->assign("person",$p[0]);
+		$this->display("Public:peopledetail");
+	}
+	public function choose()
+	{
+		$cate = $_GET["category"];
+		$p = M($cate)->select();
+		$this->assign("people",$p);
+		$this->assign("category",$cate);
+		$this->display("Public:people");
+	}
+}
+?>
