725964852763e5158c31f511f3e313316958c3c2
sxc <sheng_xc@126.com>
2012-11-25 20:45:37 +0800

    完成前台人员列表

--- dcd/DCDApp/Lib/Action/PeopleAction.class.php
+++ dcd/DCDApp/Lib/Action/PeopleAction.class.php
@@ -3,13 +3,16 @@ class PeopleAction extends BaseAction{
 	public function index()
 	{
 		$this->assign("menu","People");
+		$this->assign("category","teacher");
 		$this->display("Public:people");
 	}
-
-public function login()
+	public function choose()
 	{
-		$this->display("Public:login");
+		$cate = $_GET["category"];
+		$p = M($cate)->select();
+		$this->assign("people",$p);
+		$this->assign("category",$cate);
+		$this->display("Public:people");
 	}
-
 }
 ?>
