1c747381415f6b8b039a810fa55d7bd5a94e3b2a
DapengJiang <asuwill.jdp@gmail.com>
2012-12-01 09:28:53 +0800

    Products.html ProductAc tion.class.php

--- dcd/Admin/Lib/Action/ProductsAction.class.php
+++ dcd/Admin/Lib/Action/ProductsAction.class.php
@@ -23,7 +23,7 @@ class ProductsAction extends BaseAction{
 			$map['title'] = array('like','%'.$kmap.'%');
 		}
 		$Page -> parameter .= "keyword=".urlencode($kmap)."&";
-		$M = $M->where($map)->order(' desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		$M = $M->where($map)->order('submittime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
 		$this->assign('pages',$show);
 		$this->assign("product",$M);
 		$this->assign("dsp",$sp);
@@ -67,6 +67,23 @@ class ProductsAction extends BaseAction{
 		return $map[$page];
 	}
 	
+	static protected function tb2type($tablename)
+	{
+		$map=array(
+			"Journalpaper"=>"期刊论文",
+			"Conferencepaper"=>"会议论文",
+			"Softwareright"=>"软件",
+			"Patent"=>"专利",
+			"Techreport"=>"技术报告",
+			"Project"=>"科研项目",
+			"Thesis"=>"毕业论文"
+		);
+		if(in_array($tablename,$map))
+			return $map[$tablename];
+		else
+			return "未知";
+	}
+	
 	// 批量操作
 	public function batch(){
 		$tablename = $_GET['tablename'];
@@ -79,6 +96,13 @@ class ProductsAction extends BaseAction{
 		// addjour addconf addpatent addsoftware
 		// editjour editconf editpatent editsoftware
 		$page = $_GET['sp'];// showpage
+		$addpage=array("addjour","addconf","addpatent","addsoftware","addtr");
+		if(in_array($page,$addpage))
+		{
+			// 作者选择下拉框的数据
+			$person=M("Person")->order("name")->select();
+			$this->assign("persons",$person);
+		}
 		$this->assign("dsp",$page);
 		$this->display("Public:products");
 	}
@@ -87,19 +111,66 @@ class ProductsAction extends BaseAction{
 	{
 		$tablename=$_GET["tablename"];
 		if(!empty($_FILES)){
-			$filesinfo= $this->_uploadmul("products",false,300,400,true);
+			$filesinfo= $this->_uploadmul("products",false,300,400,true);// 附件存储在Attachments/products目录下
 		}
 		foreach($filesinfo as $num=>$file)
 		{
 			$_POST[$file["key"]]=$file["savename"];
 		}
+		$_POST["submittime"]=time();
+		for($i=1;$i<=6;$i++)
+		{
+			$var = sprintf("author%d",$i);
+			if(!empty($_POST[$var]) && $_POST[$var]!="other")
+			{
+				$author[$_POST[$var]]=$i;//$_POST[$var]是作者id，是第$i作者
+			}
+		}
+		print_r($_POST);
+		return;
 		$M=M($tablename);
 		if($M->Create())
 		{
-			if($M->add()){
-				$this->assign("jumpUrl","__URL__");
-				$this->success("添加成功！");
+			$M->startTrans();// 启动事务
+			$success = true;
+			if(($id=$M->add())!= false){
+				$Product=M("Product");//在成果表里将该成果的作者与成果关联起来
+				$Product->startTrans(); //可能要添加多条记录，确保都添加成功或失败
+				$Person=M("Person");
+				foreach($author as $userid=>$number)
+				{
+					$record["personid"]=$userid;
+					$cond["id"]=$userid;
+					$pinfo=$Person->where($cond)->find();// 根据id查找作者更多信息
+					if($pinfo)
+					{
+						$record["personname"]=$pinfo["name"];
+						$record["persontype"]=$pinfo["category"];
+						$record["productid"]=$id;
+						$record["producttype"]=$this->tb2type($tablename);
+						$record["number"]=$number;
+						if($Product->add($record)==false)
+						{
+							$success = false;
+							break;//break foreach
+						}
+					}
+				}
+				if($success)
+				{
+					$Product->commit();
+					$M->commit();//提交事务
+					$this->assign("jumpUrl","__URL__");
+					$this->success("添加成功！");
+				}
+				else
+				{
+					$Product->rollback();
+					$M->rollback();
+					$this->error("添加失败！原因：操作成果总表失败！");
+				}
 			}else{
+				$M->rollback();
 				$this->error("添加失败！");
 			}
 		}
