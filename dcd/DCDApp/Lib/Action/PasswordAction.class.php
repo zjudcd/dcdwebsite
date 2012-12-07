95efc04d42f3f83b382c95be418c06ee620a8c7c
sxc <sheng_xc@126.com>
2012-12-07 18:57:05 +0800

    搞定修改密码

--- dcd/DCDApp/Lib/Action/PasswordAction.class.php
+++ dcd/DCDApp/Lib/Action/PasswordAction.class.php
@@ -1,4 +1,22 @@
-<?php
+﻿<?php
 class PasswordAction extends Action{
+	public function index(){
+		$this->display("Public:password");
+	}
+	public function edits(){
+		$cond['personid'] = $_SESSION["userid"];
+		$person = M("person")->where($cond)->select();
+		if($person[0]['password'] == md5($_POST['oldpw']))
+		{
+			$data["personid"] = $_SESSION["userid"];
+			$data["password"] = md5($_POST['newpw']);
+			M("person")->save($data);
+			$this->success("修改成功！");
+		}
+		else
+		{
+			$this->error("原密码错误！");
+		}
+	}
 }
 ?>
