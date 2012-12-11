65bd83b7557a14ffbfde9cb904c43b30eb59be38
sxc <sheng_xc@126.com>
2012-12-11 21:42:18 +0800

    people应该好了

--- dcd/DCDApp/Lib/Action/PeopleAction.class.php
+++ dcd/DCDApp/Lib/Action/PeopleAction.class.php
@@ -1,18 +1,54 @@
-<?php
+﻿<?php
 class PeopleAction extends BaseAction{
 	public function index()
 	{
-		$this->assign("menu","People");
-		$this->assign("category","teacher");
-		$this->display("Public:people");
-	}
-	public function choose()
-	{
-		$cate = $_GET["category"];
-		$p = M($cate)->select();
-		$this->assign("menu","People");
+		if(isset($_GET["category"]))
+			$cate = $_GET["category"];
+		else
+			$cate = "teacher";
+		$Member = M("person");
+		import("ORG.Util.Page");
+		if($_POST['keyword']){
+			$kmap = $_POST['keyword'];
+			$map['name'] = array('like','%'.$kmap.'%');
+		}elseif($_GET['keyword']){
+			$kmap = $_GET['keyword'];
+			$map['name'] = array('like','%'.$kmap.'%');
+		}
+		if($cate == "teacher")
+		{
+			$map['category'] = "教师";
+			$map['status'] = "在校";
+		}	
+		else if($cate == "student")
+		{
+			$map['category'] = "学生";
+			$map['status'] = "在校";
+		}
+		else
+		{
+			$map['category'] = "学生";
+			$map['status'] = "离校";
+		}
+		$count = $Member->where($map)->count();
+		$Page = new Page($count,12);
+		$Page -> parameter .= "keyword=".urlencode($kmap)."&";
+		$show = $Page->show();
+		$pp = $Member->where($map)->order('personid desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		if($cate == "grastudent")
+			$type = "student";
+		else
+			$type = $cate;
+		$p = Array();
+		foreach($pp as $k => $v)
+		{
+			$cond["id"] = $v["personid"];
+			$result = M($type)->where($cond)->select();
+			array_push($p,$result[0]);
+		}
+		$this->assign("category",$type);
 		$this->assign("people",$p);
-		$this->assign("category",$cate);
+		$this->assign("menu","People");
 		$this->display("Public:people");
 	}
 }
