ea9c38877ffc154ef62bfc4d0a41fed1dda2869b
sxc <sheng_xc@126.com>
2012-12-18 22:38:31 +0800

    搞定论文详情页面

--- dcd/DCDApp/Lib/Action/PaperAction.class.php
+++ dcd/DCDApp/Lib/Action/PaperAction.class.php
@@ -1,6 +1,14 @@
 <?php
 class PaperAction extends Action{
 	public function index(){
+		$type = $_GET["type"];
+		if($type == "jour")
+			$table = "journalpaper";
+		else
+			$table = "conferencepaper";
+		$cond["id"] = $_GET["id"];
+		$p = M($table)->where($cond)->select;
+		$this->assign(paper,$p[0]);
 		$this->display("Public:paper");
 	}
 }
